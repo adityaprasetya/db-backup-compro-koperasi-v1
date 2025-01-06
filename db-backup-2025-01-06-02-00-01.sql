9d3eec3071207b1868012b47de715885347e623d
Aditya <adityaprasetyasuryana@gmail.com>
2025-01-06 02:00:02 +0000

    Backup database app_compro_koperasi tanggal 2025-01-06-02-00-01

---
+++ db-backup-2025-01-06-02-00-01.sql
@@ -0,0 +1,223 @@
+-- MySQL dump 10.13  Distrib 8.0.40, for Linux (x86_64)
+--
+-- Host: localhost    Database: app_compro_koperasi
+-- ------------------------------------------------------
+-- Server version	8.0.40-0ubuntu0.22.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `blog`
+--
+
+DROP TABLE IF EXISTS `blog`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `blog` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `slug` varchar(255) DEFAULT NULL,
+  `content` text NOT NULL,
+  `author_id` int NOT NULL,
+  `status` enum('draft','published','archived') DEFAULT 'draft',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `published_at` timestamp NULL DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `slug` (`slug`),
+  KEY `author_id` (`author_id`),
+  CONSTRAINT `blog_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `blog`
+--
+
+LOCK TABLES `blog` WRITE;
+/*!40000 ALTER TABLE `blog` DISABLE KEYS */;
+INSERT INTO `blog` VALUES (7,'judul test gambar','judul-test-gambar','konten test gambar',1,'draft','2024-12-04 07:35:45','2024-12-04 09:19:21',NULL,'221205 A koperasi-konsumsi-adalah 1.jpg'),(8,'judul test gambar 2','judul-test-gambar-2','konten test gambar 2',1,'draft','2024-12-04 08:09:43','2024-12-04 09:19:52',NULL,'4119346130.jpg');
+/*!40000 ALTER TABLE `blog` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `galeri`
+--
+
+DROP TABLE IF EXISTS `galeri`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `galeri` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `galeri`
+--
+
+LOCK TABLES `galeri` WRITE;
+/*!40000 ALTER TABLE `galeri` DISABLE KEYS */;
+INSERT INTO `galeri` VALUES (1,'JkU0toAmaxPzcK5leZytZV670AL9uwDzf3vlUKOR.jpg','2024-12-04 16:58:42','2024-12-04 17:06:53'),(2,'sp.jpeg','2024-12-04 17:11:10','2024-12-04 17:11:10'),(4,'4119346130.jpg','2024-12-04 17:17:47','2024-12-04 17:17:47'),(5,'221205 A koperasi-konsumsi-adalah 1.jpg','2024-12-04 17:18:10','2024-12-04 17:18:10');
+/*!40000 ALTER TABLE `galeri` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `pembiayaan`
+--
+
+DROP TABLE IF EXISTS `pembiayaan`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `pembiayaan` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `pembiayaan`
+--
+
+LOCK TABLES `pembiayaan` WRITE;
+/*!40000 ALTER TABLE `pembiayaan` DISABLE KEYS */;
+INSERT INTO `pembiayaan` VALUES (1,'pembiayaan-v3.png','2024-12-07 10:55:33','2024-12-07 13:10:08');
+/*!40000 ALTER TABLE `pembiayaan` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `promosi`
+--
+
+DROP TABLE IF EXISTS `promosi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `promosi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `promosi`
+--
+
+LOCK TABLES `promosi` WRITE;
+/*!40000 ALTER TABLE `promosi` DISABLE KEYS */;
+INSERT INTO `promosi` VALUES (1,'promosi.jpg','2024-12-07 11:53:21','2024-12-07 11:53:21'),(2,'promosi-2.jpg','2024-12-07 11:53:59','2024-12-07 11:53:59'),(3,'promosi-3.jpg','2024-12-07 11:56:37','2024-12-07 11:56:37'),(4,'promosi-4.jpg','2024-12-07 11:56:58','2024-12-07 11:56:58'),(5,'promosi-5.jpg','2024-12-07 11:57:10','2024-12-07 11:57:10'),(6,'promosi-6.jpg','2024-12-07 11:57:21','2024-12-07 11:57:21');
+/*!40000 ALTER TABLE `promosi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `simulasi`
+--
+
+DROP TABLE IF EXISTS `simulasi`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `simulasi` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `simulasi`
+--
+
+LOCK TABLES `simulasi` WRITE;
+/*!40000 ALTER TABLE `simulasi` DISABLE KEYS */;
+INSERT INTO `simulasi` VALUES (1,'simulasi.png','2024-12-07 11:34:25','2024-12-07 11:34:25');
+/*!40000 ALTER TABLE `simulasi` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `sliders`
+--
+
+DROP TABLE IF EXISTS `sliders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `sliders` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `image` varchar(255) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `sliders`
+--
+
+LOCK TABLES `sliders` WRITE;
+/*!40000 ALTER TABLE `sliders` DISABLE KEYS */;
+INSERT INTO `sliders` VALUES (1,'bg-pendaftaran.jpg','2024-12-04 18:21:18','2024-12-04 18:21:18'),(2,'1725253073.jpg','2024-12-04 18:21:55','2024-12-04 18:21:55'),(3,'mykey.jpg','2024-12-04 18:22:14','2024-12-04 18:28:50');
+/*!40000 ALTER TABLE `sliders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `email_verified_at` timestamp NULL DEFAULT NULL,
+  `password` varchar(255) NOT NULL,
+  `remember_token` varchar(100) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'John Doe','john.doe@example.com',NULL,'$2y$10$KPfY1ZNYwYQTo.Z/TBYJiua98CAJLZNIrM3tL49VtQYLa8HsnFqMu',NULL,'2024-12-03 12:59:17','2024-12-03 12:59:17'),(2,'Jane Doe','jane.doe@example.com',NULL,'$2y$10$DRu3KBiSHCr9yQ3qTQ1bjehYvdZImG2IAfh5fe.hPG2TbZ8zA.Ppa',NULL,'2024-12-03 14:46:51','2024-12-03 14:46:51');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-01-06  2:00:01
